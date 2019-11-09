b4311539283334ed713fbcabb27e0d198d67b8bb
Nicolas EYRAUD <nieyraud@e1r9p10.42.fr>
2019-11-09 17:58:21 +0100

    norming

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,39 +6,40 @@
 /*   By: nieyraud <nieyraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/27 09:42:48 by nieyraud          #+#    #+#             */
-/*   Updated: 2019/11/09 16:25:39 by nieyraud         ###   ########.fr       */
+/*   Updated: 2019/11/09 17:58:04 by nieyraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
-#include <stdio.h>
 
 static int		flags_parse(char *flags, va_list ap, size_t len)
 {
 	int		count;
 
 	count = 0;
-	count += (flags[len - 1] == 's' ? ft_parse_string(flags, ap) : 0);
-	count += (flags[len - 1] == 'd' ? ft_parse_digit(flags, ap) : 0);
-	count += (flags[len - 1] == 'i' ? ft_parse_digit(flags, ap) : 0);
-	count += (flags[len - 1] == 'c' ? ft_parse_char(flags, ap) : 0);
-	count += (flags[len - 1] == '%' ? ft_parse_percent(flags, ap) : 0);
-	count += (flags[len - 1] == 'p' ? ft_parse_ptr(flags, ap) : 0);
-	count += (flags[len - 1] == 'x' ? ft_parse_hexa(flags, ap, 'x') : 0);
-	count += (flags[len - 1] == 'X' ? ft_parse_hexa(flags, ap, 'X') : 0);
-	count += (flags[len - 1] == 'u' ? ft_parse_unsigned(flags, ap) : 0);
+	if (flags[len - 1] == 's')
+		count += ft_parse_string(flags, ap);
+	else if (flags[len - 1] == 'd' || flags[len - 1] == 'i')
+		count += ft_parse_digit(flags, ap);
+	else if (flags[len - 1] == 'c')
+		count += ft_parse_char(flags, ap);
+	else if (flags[len - 1] == '%')
+		count += ft_parse_percent(flags, ap);
+	else if (flags[len - 1] == 'p')
+		count += ft_parse_ptr(flags, ap);
+	else if (flags[len - 1] == 'x' || flags[len - 1] == 'X')
+		count += ft_parse_hexa(flags, ap, flags[len - 1]);
+	else if (flags[len - 1] == 'u')
+		count += ft_parse_unsigned(flags, ap);
 	return (count);
 }
 
-int		ft_printf(const char *str, ...)
+static void		ft_browse_string(char *str, va_list ap, int *count)
 {
-	va_list ap;
-	char	*data_type;
+	int		i;
 	char	*flags;
-	size_t		i;
-	int count = 0;
+	char	*data_type;
 
-	va_start(ap, str);
 	data_type = "%cspdiuxX";
 	while (*str)
 	{
@@ -58,5 +59,15 @@ int		ft_printf(const char *str, ...)
 		}
 		str += i;
 	}
+}
+
+int				ft_printf(const char *str, ...)
+{
+	va_list ap;
+	int		count;
+
+	va_start(ap, str);
+	ft_browse_string(str, ap, &count);
+	va_end(ap);
 	return (count);
 }
